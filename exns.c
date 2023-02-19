8dc94d7f3389f4a55db3d0b41df785abac770e08
Abhishek Mishra <abhishekmishra3@gmail.com>
2023-02-20 00:32:20 +0530

    added namespaces list

--- exns.c
+++ exns.c
@@ -3,9 +3,58 @@
 
 #include <unistd.h>
 
+// Namespace types returned by NS_GET_NSTYPE.
+#define CLONE_NEWNS       0x00020000
+#define CLONE_NEWCGROUP   0x02000000
+#define CLONE_NEWUTS      0x04000000
+#define CLONE_NEWIPC      0x08000000
+#define CLONE_NEWUSER     0x10000000
+#define CLONE_NEWPID      0x20000000
+#define CLONE_NEWNET      0x40000000
+#define CLONE_NEWTIME     0x00000080
+#define NAMESPACES_LEN    8
+
+typedef struct {
+    int flag;
+    char* name;
+} namespace_t;
+
+namespace_t NAMESPACES[] = {
+    {
+        CLONE_NEWNS, "mnt"
+    },
+    {
+        CLONE_NEWCGROUP, "cgroup"
+    },
+    {
+        CLONE_NEWUTS, "uts"
+    },
+    {
+        CLONE_NEWIPC, "ipc"
+    },
+    {
+        CLONE_NEWUSER, "user"
+    },
+    {
+        CLONE_NEWPID, "pid"
+    },
+    {
+        CLONE_NEWNET, "net"
+    },
+    {
+        CLONE_NEWTIME, "time"
+    }
+};
+
+
 int main(int argc, char* argv[])
 {
     printf("exns\n");
     printf("PID = %d, PPID = %d\n", getpid(), getppid());
+
+    for(int i = 0; i < NAMESPACES_LEN; i++)
+    {
+        printf("%s\n", NAMESPACES[i].name);
+    }
     exit(0);
 }
