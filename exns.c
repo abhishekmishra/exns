d60def3506f09b6c00a380a45eb7680f65c16778
Abhishek Mishra <abhishekmishra3@gmail.com>
2023-02-20 01:33:43 +0530

    comments, path string is statically allocated, functions to get ns flag and name

--- exns.c
+++ exns.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <string.h>
 
 #include <unistd.h>
 
@@ -20,12 +21,27 @@
 // NS paths construction
 #define PROC_DIR          "/proc"
 
+/**
+ * Struct representing a namespace type in linux.
+ * It has the flag used for namespace APIS, and the name of the namespace
+ * in paths.
+ */
 typedef struct {
     int flag;
     char* name;
 } namespace_t;
 
-namespace_t NAMESPACES[] = {
+// Globals
+
+/** path string for use in all methods */
+char EXNS_PATH_STR[PATH_MAX] = { '\0' };
+
+/** list of available namespaces on the system, NULL terminated array of
+ * strings*/
+char* sys_ns[8] = { NULL };
+
+/** All namespaces pairs (API flag, name) possible in linux */
+namespace_t ALL_NS[] = {
     {
         CLONE_NEWNS, "mnt"
     },
@@ -52,23 +68,70 @@ namespace_t NAMESPACES[] = {
     }
 };
 
+/**
+ * Get the ns flag given ns name
+ * (return -1 if now found)
+ *
+ * @param name name of ns
+ * @return flag
+ */
+int get_ns_flag_by_name(char* name)
+{
+    if(name == NULL)
+    {
+        return -1;
+    }
+
+    for(int i = 0; i < NAMESPACES_LEN; i++)
+    {
+        if(strcmp(ALL_NS[i].name, name) == 0)
+        {
+            return ALL_NS[i].flag;
+        }
+    }
+    return -1;
+}
+
+/**
+ * Get the ns name given flag
+ * (return NULL if not found)
+ *
+ * @param flag flag of ns
+ * @return name of ns
+ */
+char* get_ns_name_by_flag(int flag)
+{
+    for(int i = 0; i < NAMESPACES_LEN; i++)
+    {
+        if(ALL_NS[i].flag == flag)
+        {
+            return ALL_NS[i].name;
+        }
+    }
+    return NULL;
+}
+
+void get_ns_symlink_list()
+{
+    char* self_pid_path = "/proc/self/ns/";
+    for(int i = 0; i < NAMESPACES_LEN; i++)
+    {
+        snprintf(EXNS_PATH_STR, PATH_MAX, "%s%s", self_pid_path, ALL_NS[i].name);
+        printf("%s\n", EXNS_PATH_STR);
+    }
+}
+
 
 int main(int argc, char* argv[])
 {
-    char* path_str = (char*) calloc(sizeof(char), PATH_MAX);
-    if(path_str == NULL)
-    {
-        fprintf(stderr, "Unable to allocate path string!\n");
-        exit(-1);
-    }
-    path_str[0] = '\0';
-
     printf("exns\n");
     printf("PID = %d, PPID = %d\n", getpid(), getppid());
 
     for(int i = 0; i < NAMESPACES_LEN; i++)
     {
-        printf("%s\n", NAMESPACES[i].name);
+        printf("%s\n", ALL_NS[i].name);
     }
+
+    get_ns_symlink_list();
     exit(0);
 }
