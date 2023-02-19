dcdeb2671dbb40442ca5813546b455c208908686
Abhishek Mishra <abhishekmishra3@gmail.com>
2023-02-20 03:56:46 +0530

    load system available namespaces using directory listing of /proc/self/ns

--- exns.c
+++ exns.c
@@ -6,6 +6,7 @@
 #include <string.h>
 
 #include <unistd.h>
+#include <dirent.h>
 
 // Namespace types returned by NS_GET_NSTYPE.
 #define CLONE_NEWNS       0x00020000
@@ -38,7 +39,7 @@ char EXNS_PATH_STR[PATH_MAX] = { '\0' };
 
 /** list of available namespaces on the system, NULL terminated array of
  * strings*/
-char* sys_ns[8] = { NULL };
+char* EXNS_SYS_NS[NAMESPACES_LEN + 1] = { NULL };
 
 /** All namespaces pairs (API flag, name) possible in linux */
 namespace_t ALL_NS[] = {
@@ -111,27 +112,103 @@ char* get_ns_name_by_flag(int flag)
     return NULL;
 }
 
-void get_ns_symlink_list()
+/**
+ * Updates the list of system namespaces in EXNS_SYS_NS.
+ * This is done to ensure only namespaces available in the current
+ * linux kernel are used while listing namespaces.
+ *
+ * @return error code
+ */
+int get_ns_symlink_list()
 {
     char* self_pid_path = "/proc/self/ns/";
-    for(int i = 0; i < NAMESPACES_LEN; i++)
+    DIR *dirp;
+    struct dirent *dent;
+    int flag;
+    int ns_count = 0;
+
+    // open the self pid directory
+    dirp = opendir(self_pid_path);
+    if (dirp == NULL)
     {
-        snprintf(EXNS_PATH_STR, PATH_MAX, "%s%s", self_pid_path, ALL_NS[i].name);
-        printf("%s\n", EXNS_PATH_STR);
+        fprintf(stderr, "Open directory failed for path %s.\n", self_pid_path);
+        return -1;
     }
+
+    // loop till no further directory entries are found
+    for (;;)
+    {
+        // read the next directory entry
+        dent = readdir(dirp);
+
+        // if there is no directory entry returned, exit the loop
+        if (dent == NULL)
+        {
+            break;
+        }
+
+        // ignore "." and ".." directory entries as they cannot be namespaces
+        if (strcmp(dent->d_name, ".") == 0 || strcmp(dent->d_name, "..") == 0)
+        {
+            continue;               /* Skip . and .. */
+        }
+
+        // lookup directory entry name to check if it is a namespace name
+        flag = get_ns_flag_by_name(dent->d_name);
+        //printf("%s/%s, flag = %d\n", self_pid_path, dent->d_name, flag);
+
+        // if the directory entry name is a namespace then
+        // get the program static string for the entry
+        // and append it to the known namespaces list
+        // note that the string provided by directory entry is not
+        // owned by this program therefore ns name has to be looked up again.
+        if(flag != -1)
+        {
+            EXNS_SYS_NS[ns_count] = get_ns_name_by_flag(flag);
+            ns_count += 1;
+        }
+    }
+
+    // close the directory and return
+    if (closedir(dirp) == -1)
+    {
+        fprintf(stderr, "Failed to close directory.\n");
+        return -1;
+    }
+
+    return 0;
 }
 
 
 int main(int argc, char* argv[])
 {
-    printf("exns\n");
-    printf("PID = %d, PPID = %d\n", getpid(), getppid());
+    int res;
 
-    for(int i = 0; i < NAMESPACES_LEN; i++)
+    printf("Process PID = %d, PPID = %d\n", getpid(), getppid());
+
+    //for(int i = 0; i < NAMESPACES_LEN; i++)
+    //{
+    //    printf("%s\n", ALL_NS[i].name);
+    //}
+
+    // load the system available namespaces
+    res = get_ns_symlink_list();
+
+    // if there was an error getting list of system namespaces
+    if(res != 0)
     {
-        printf("%s\n", ALL_NS[i].name);
+        exit(-1);
     }
 
-    get_ns_symlink_list();
+    printf("Namespace types available on this system are:\n");
+    for(int i = 0; i < NAMESPACES_LEN+1; i++)
+    {
+        if(EXNS_SYS_NS[i] == NULL)
+        {
+            break;
+        }
+        printf("%s\n", EXNS_SYS_NS[i]);
+    }
+
     exit(0);
 }
